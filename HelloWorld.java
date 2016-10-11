93d7a47cc34005e10ce980d92096ed3c7ee743c2
victoria <1491693608@qq.com>
2016-10-11 16:55:05 +0800

    测试TestSourceTree

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/*基本语法：
+	大小写敏感：Java大小写敏感，
+	类名：类名的首字母大写，类名由若干单词组成，应该每个单词首字母大写，MyFirstJavaClass.
+	方法名：所有的方法名都应该以小写字母开头。如果方法名含有若干单词，则后面的每个单词首字母大写。
+	源文件名：源文件名必须和类名相同，当保存文件时，应该使用类名作为文件名保存。
+	主方法入口：所有的Java程序由public static void main(String []args)方法开始执行。
+*/
+public class HelloWorld {
+    public static void main(String []args) {
+        System.out.println("Hello World");
+    }
+}
+
